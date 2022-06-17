46651072c2b3c06659688f82d5cbaad7db437311
Karthick2299 <106722215+Karthick2299@users.noreply.github.com>
2022-06-17 18:22:58 +0530

    switch case example

---
+++ JavaScript-Examples/switchCaseInJs.js
@@ -0,0 +1,27 @@
+let day;
+
+switch (new Date().getDay()){
+    case 0:
+        day = "monday";
+        break;
+    case 1:
+        day = "Tuesday";
+        break;
+    case 2:
+        day = "Wednesday";
+        break;
+    case 3:
+        day = "Thursday";
+        break;
+    case 4:
+        day = "Friday";
+        break;
+    case 5:
+        day = "Saturday";
+        break;
+    case 6:
+        day = "Sunday";
+        break;
+}
+
+console.log(day);

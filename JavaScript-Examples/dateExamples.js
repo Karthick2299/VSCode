b3d3db6c98226685ce72b6209d199aec2e090367
Karthick2299 <106722215+Karthick2299@users.noreply.github.com>
2022-06-20 18:09:36 +0530

    date Examples added

---
+++ JavaScript-Examples/dateExamples.js
@@ -0,0 +1,67 @@
+//Date Example 1
+
+// const date = new Date();
+
+// console.log("Time : " + date.getHours() + " : " + date.getMinutes() + " : " + date.getSeconds());
+
+
+//Date Example 2
+
+// const date = new Date();
+
+// console.log(date);
+
+
+//Date Example 3
+
+// const date = new Date("Mar 22 1999");
+// console.log(date);
+
+//Date Example 4
+
+// const date = Date.parse("Mar 22 1999");
+// console.log(date);
+
+// const newDate  = new Date(date);
+// console.log(newDate);
+
+
+
+/*
+getFullYear();
+getMonth();
+getDate();
+getHours();
+getMinutes();
+getSeconds();
+getMilliseconds();
+getTime();
+getDay();
+Date.now();
+*/
+
+//These are the methods in Date
+
+//Date Example - getMethods()
+
+// const date = new Date();
+
+// console.log("year : " + date.getFullYear());
+// console.log("Month : " + date.getMonth());
+// console.log("Date : " + date.getDate());
+// console.log("Hours : " + date.getHours());
+// console.log("Minutes : " + date.getMinutes());
+// console.log("MilliSeconds : " + date.getMilliseconds());
+// console.log("Time : " + date.getTime());
+// console.log("Day : " + date.getDay());
+// console.log("Date now : " + Date.now())
+
+
+//Date Example 5
+
+// const days = ["Monday","Tuesday", "Wednesday", "Thursday", "Friday","Saturday", "Sunday"];
+
+// const date = new Date();
+// let day  = days[date.getDay()];
+
+// console.log(day);

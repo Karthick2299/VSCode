fe5672685bdb5e6c243d098c5574060a756d7ee5
Karthick2299 <106722215+Karthick2299@users.noreply.github.com>
2022-06-21 18:05:43 +0530

    examples added

--- JavaScript-Examples/object_Examples.js
+++ JavaScript-Examples/object_Examples.js
@@ -1,9 +1,35 @@
+// const person = {
+//     f_Name : "karthick",
+//     L_name : "selvam",
+//     eyeColor: "Brown"
+// };
+
+// const myArray = Object.values(person);
+// console.log(myArray);
+
+// let myString = JSON.stringify(person);
+
+// console.log(myString);
+
+
+//StringyFy Arrays Example
+
+// const cars = ["maserati","audi","mercedes benz"];
+
+// let myString = JSON.stringify(cars);
+
+// console.log(myString);
+
+//Getter and Setter Examples
+
 const person = {
-    f_Name : "karthick",
-    L_name : "selvam",
-    eyeColor: "Brown"
+    f_name : "karthick",
+    l_name : "selvam",
+    language : "en",
+    get lang(){
+        return this.language;
+    }
+
 };
 
-const myArray = Object.values(person);
-console.log(myArray);
-
+console.log(person.lang);

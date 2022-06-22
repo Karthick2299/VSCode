813d10cc3b6756d2668afae9a2e58c580ddf4f8f
Karthick2299 <106722215+Karthick2299@users.noreply.github.com>
2022-06-22 14:27:55 +0530

    class Examples are added

--- JavaScript-Examples/class_js.js
+++ JavaScript-Examples/class_js.js
@@ -22,3 +22,20 @@
 // const callConstructor = new Example_name("karthick","selvam");
 
 // console.log(callConstructor.first + " " + callConstructor.last);
+
+class bike{
+    constructor(name,year){
+        this.name = name;
+        this.year = year;
+    }
+    ageOfBike(){
+        let date = new Date();
+        return date.getFullYear() - this.year;
+    }
+}
+
+let myBike  = new bike("CD100",1982);
+let myCar = new bike("RollsRoyce",2018);
+
+console.log("My bike Name is : " + myBike.name + " and it is " + myBike.ageOfBike() + " years old ");
+console.log("My Car Name is : " + myCar.name + " and it is " + myCar.ageOfBike() + " years old ");

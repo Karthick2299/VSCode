d794cef7dc3922d656b82a2487d7cde76a301c55
Karthick2299 <106722215+Karthick2299@users.noreply.github.com>
2022-06-23 09:40:10 +0530

    call back function Example added

--- JavaScript-Examples/callBack_Examples.js
+++ JavaScript-Examples/callBack_Examples.js
@@ -1,13 +1,54 @@
 //callback function Example 
 
-function displayer(getInput){
-    console.log("printing statement from displayer method " + getInput);
+// function displayer(getInput){
+//     console.log("printing statement from displayer method " + getInput);
+// }
+
+// function addition(first_number, second_number, callbackFunction){
+//     let sum = first_number + second_number;
+//     callbackFunction(sum);
+
+// }
+
+// addition(3,4,displayer);
+
+//Callback function Example 2
+
+// function myResult(){
+//     let exampleString = "karthick";
+//     displayMyName(exampleString);
+// }
+
+// function displayMyName(result){
+//     console.log(" Hi " + result);
+// }
+
+// myResult();
+
+//callBack function Example 3
+
+function mainFunction(){
+
+    let exampleString = "Karthick - comes from mainFunction";
+    first_function(exampleString);
+
 }
 
-function addition(first_number, second_number, callbackFunction){
-    let sum = first_number + second_number;
-    callbackFunction(sum);
-
+function first_function(result){
+    let anotherVariable = result;
+    console.log("first function called");
+    second_function(anotherVariable);
 }
 
-addition(3,4,displayer);
+function second_function(InputFromFirst_Function){
+    let second_variable = InputFromFirst_Function;
+    console.log("second function called");
+    displayer(second_variable);
+}
+
+function displayer(result){
+    console.log("finally displayer method called");
+    console.log(result);
+}
+
+mainFunction();

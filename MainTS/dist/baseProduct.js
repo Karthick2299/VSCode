df85a8b6e9111df11c3fe77ac98235567239d90b
Karthick2299 <106722215+Karthick2299@users.noreply.github.com>
2022-08-01 09:11:10 +0530

    webpack Examples are added

---
+++ MainTS/dist/baseProduct.js
@@ -0,0 +1,4 @@
+import { Product } from "./productModel";
+const p1 = new Product("hello", 0.99);
+console.log(p1.getInformation());
+//# sourceMappingURL=baseProduct.js.map
---
+++ MainTS/src/baseProduct.ts
@@ -0,0 +1,6 @@
+import { Product } from "./productModel";
+
+
+const p1 = new Product("hello",0.99);
+
+console.log(p1.getInformation())

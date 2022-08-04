e6a638f1554963252b788038c46d794c3108b7cc
Karthick2299 <106722215+Karthick2299@users.noreply.github.com>
2022-08-04 16:32:56 +0530

    generic and Inheritance Examples are added

--- JavaScript-Examples/TypeScript/Ts/genericExample.ts
+++ JavaScript-Examples/TypeScript/Ts/genericExample.ts
@@ -1,8 +1,34 @@
-function identity<T>(arg: T): T{
-    return arg;
-}
+// function identityFunction<T>(arg: T): T{
+//     return arg;
+// }
 
-let output1 = identity<string>("hello");
-let output2 = identity<number>(100);
-console.log(output1);
-console.log(output2);
+// let output1 = identityFunction<string>("hello");
+// let output2 = identityFunction<number>(100);
+// console.log(output1);
+// console.log(output2);
+
+
+//Generic Example 2
+
+// class studentInfo<T, U>{
+//     private Id: T;
+//     private Name: U;
+
+//     constructor(id: T, name: U){
+//         this.Id = id;
+//         this.Name = name;
+//     }
+
+//     display(): void{
+//         console.log(`id = ${this.Id}, Name = ${this.Name}`);
+//     }
+// }
+
+// const a1 = new studentInfo(1, "hello");
+// a1.display();
+
+//Generic Example 3
+
+class Student{
+    
+}

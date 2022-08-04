7f5a9ffd8f52e072766b552e22b4d27ba61c7003
Karthick2299 <106722215+Karthick2299@users.noreply.github.com>
2022-08-04 12:42:17 +0530

    Binary Search Example added

---
+++ DataStructures/BinarySearch.js
@@ -0,0 +1,31 @@
+//create an array
+let array = [1, 3, 4, 5, 7, 22, 25, 76];
+let target = 5;
+
+let iterativeFunction = function(array, target){
+    let start = 0, end = array.length-1;
+
+    while(start <= end){
+        let mid = Math.floor((start + end) / 2);
+
+        if(array[mid] === target){
+            return true;
+        }
+        else if(array[mid] < target){
+            start = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
+    }
+
+    return false;
+}
+
+
+if(iterativeFunction(array, target, 0, array.length-1)){
+    document.write("Element Found");
+}
+else{
+    document.write("Element not Found")
+}
---
+++ JavaScript-Examples/TypeScript/Ts/genericExample.js
@@ -0,0 +1,7 @@
+function identity(arg) {
+    return arg;
+}
+var output1 = identity("hello");
+var output2 = identity(100);
+console.log(output1);
+console.log(output2);
